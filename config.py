65c7be7d322d98ce4faf3ed785a7d8c665bc54ba
hmzhmomad200-crypto <hmzhmomad200@gmail.com>
2026-05-24 04:26:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "8299852876:AAHZu-ZdQydiRNK9zOMqdaxrdS80XS4C4ao"
 ADMIN_IDS = [6043225431]  # ضع ID الأدمن
 
 LTC_WALLET = "ltc1qwzrgzkze5aey9wg2jp2vn8y4933xzuqjmutxnl"
-BASE_URL = "bot1-production-e7f7.up.railway.app"
+BASE_URL = "bot1-production-a903.up.railway.app"
 API_URL = "https://litepay.ch/api/receive"
 
 PRICE_LTC = 0.05  # السعر الافتراضي لكل منتج
